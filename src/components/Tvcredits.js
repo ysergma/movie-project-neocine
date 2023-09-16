48a797185f1337b9ed24f2b72ded7cb52b9fe68b
Youssouf Sergma <sil3nthilldz@gmail.com>
2023-09-16 01:38:41 +0100

    feat(component): add Tv credits to the actors page
    add Tv credits to the actors page(this shows other tv shows of the same actor)
    
    "fix #6"

---
+++ src/components/Tvcredits.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react"
+import { fetcher } from "../../util/API"
+import Cards from "../components/Cards"
+
+const Tvcredits = ({ actor_id }) => {
+  const [credits, setCredits] = useState([])
+  const apiRoute = `person/${actor_id}/tv_credits?language=en-US `
+  const fetchCredits = async () => {
+    const data = await fetcher(apiRoute)
+    console.log(data.cast)
+    setCredits(data.cast)
+  }
+  useEffect(() => {
+    fetchCredits()
+  }, [])
+  {
+    if (credits) {
+      return (
+        <div>
+          <h2>Similar Tv Shows</h2>
+          {credits.map((credit, index) => (
+            <div key={index}>
+              {credit.title}
+              <Cards
+                name={credit.title}
+                overview={credit.overview}
+                poster_path={credit.poster_path}
+              />
+            </div>
+          ))}
+        </div>
+      )
+    } else {
+      return ""
+    }
+  }
+}
+
+export default Tvcredits
