d919accb4e2aac3bbe2cd93f329e3a96cf8a6ab5
Youssouf Sergma <sil3nthilldz@gmail.com>
2023-09-19 12:53:25 +0100

    footer edited

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,31 @@
-import styles from "@/styles/footer.module.css"
-import GitHubIcon from "@mui/icons-material/GitHub"
+import styles from "@/styles/footer.module.css";
+import GitHubIcon from "@mui/icons-material/GitHub";
+
+const TEAM_LINKEDIN_PROFILES = {
+  "Sidahmed slikh": "https://www.linkedin.com/in/sidahmed-zinedine",
+  "Benarba Tewfik": "https://www.linkedin.com/in/mohammed-tewfik-benarba",
+  "Bouchra ikram aboura": "https://www.linkedin.com/in/bouchra-ikram-aboura-1750b5169/",
+  "Youssef sergma": "https://www.linkedin.com/in/sergma",
+  "imane omari": "https://www.linkedin.com/in/iman-omari",
+  "Melissa Sidi Said": "https://www.linkedin.com/in/milyssa-sidisaid-46170a232/",
+};
+const TEAM_GITHU_PROFILES = {
+  "Sidahmed slikh": "https://github.com/Zino0031",
+  "Benarba Tewfik": "https://github.com/Ben-Tewfik",
+  "Bouchra ikram aboura": "https://github.com/ikoworld",
+  "Youssef sergma": "https://github.com/ysergma",
+  "imane omari": "https://github.com/iman-om",
+  "Melissa Sidi Said": "https://github.com/milyssaSidisaid",
+};
+
+
 export default function Footer() {
+  const [selectedTeamMember, setSelectedTeamMember] = useState(null);
+
+  const handleTeamMemberClick = (teamMember) => {
+    setSelectedTeamMember(teamMember);
+  };
+
   return (
     <div className={styles.footerContainer}>
       <div>
@@ -10,61 +35,29 @@ export default function Footer() {
       </div>
       <div className={styles.teamBox}>
         <h1>team</h1>
-        <h2>Sidahmed slikh</h2>
-        <h2>Benarba Tewfik</h2>
-        <h2>Bouchra ikram aboura</h2>
-        <h2>Youssef sergma</h2>
-        <h2>imane omari</h2>
-        <h2>Melissa Sidi Said</h2>
-      </div>
-      <div className={styles.linkedinBox}>
-        <h1>linkedin</h1>
-        <a
-          href="https://www.linkedin.com/in/sidahmed-zinedine"
-          target={"_blank"}
-        >
-          linkedin/Sidahmed slikh
-        </a>
-        <a href="www.linkedin.com/in/mohammed-tewfik-benarba" target={"_blank"}>
-          linkedin/Benarba Tewfik
-        </a>
-        <a
-          href="https://www.linkedin.com/in/bouchra-ikram-aboura-1750b5169/"
-          target={"_blank"}
-        >
-          linkedin/Bouchra ikram aboura
-        </a>
-        <a href="https://www.linkedin.com/in/sergma">linkedin/Youssef sergma</a>
-        <a href="https://www.linkedin.com/in/iman-omari" target={"_blank"}>
-          linkedin/imane omari
-        </a>
-        <a
-          href="https://www.linkedin.com/in/milyssa-sidisaid-46170a232/"
-          target={"_blank"}
-        >
-          linkedin/Melissa Sidi Said
-        </a>
+        <ul>
+          {Object.keys(TEAM_MEMBER_PROFILES).map((teamMember) => (
+            <li key={teamMember} onClick={() => handleTeamMemberClick(teamMember)}>
+              {teamMember}
+            </li>
+          ))}
+        </ul>
       </div>
       <div className={styles.githubBox}>
         <h1>github</h1>
-        <a href="https://github.com/Zino0031" target={"_blank"}>
-          Zino0031
-        </a>
-        <a href="https://github.com/Ben-Tewfik" target="_blank">
-          Ben-Tewfik
-        </a>
-        <a href="https://github.com/ikoworld" target={"_blank"}>
-          ikoworld
-        </a>
-        <a href="https://github.com/ysergma" target={"_blank"}>
-          ysergma
-        </a>
-        <a href="https://github.com/iman-om" target={"_blank"}>
-          iman-om
-        </a>
-        <a href="https://github.com/milyssaSidisaid" target={"_blank"}>
-          milyssaSidisaid
-        </a>
+        {selectedTeamMember && (
+          <a href={TEAM_GITHU_PROFILES[selectedTeamMember]} target={"_blank"}>
+            {selectedTeamMember}
+          </a>
+        )}
+      </div>
+      <div className={styles.linkedinBox}>
+        <h1>linkedin</h1>
+        {selectedTeamMember && (
+          <a href={TEAM_LINKEDIN_PROFILES[selectedTeamMember]} target={"_blank"}>
+            {selectedTeamMember}
+          </a>
+        )}
       </div>
       <div className={styles.githubRepoBox}>
         <p style={{ color: "#F5C518" }}>source code:</p>
@@ -76,5 +69,5 @@ export default function Footer() {
         </a>
       </div>
     </div>
-  )
+  );
 }
